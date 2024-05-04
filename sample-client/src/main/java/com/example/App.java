935718f141a144f7add22174f9c9f8db0a792ca8
Tim <tim_macpherson@yahoo.co.uk>
2024-05-04 16:05:10 +0100

    Update App.java
    cuts

--- sample-client/src/main/java/com/example/App.java
+++ sample-client/src/main/java/com/example/App.java
@@ -1,25 +1,6 @@
 package com.example;
 
-
 import com.google.gwt.core.client.EntryPoint;
-
-/*
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-*/
 import org.dominokit.domino.ui.utils.ElementsFactory;
 import org.dominokit.domino.ui.style.DominoCss;
 import org.dominokit.domino.ui.cards.Card;
@@ -32,13 +13,13 @@ import org.dominokit.domino.ui.tree.TreeItem;
 
 public class App implements EntryPoint, ElementsFactory, DominoCss {
 
-	public void onModuleLoad() {
+  public void onModuleLoad() {
     
     var layout = AppLayout.create("Domino-ui starter");
     
     layout.withLeftDrawer((parent, drawer) -> {
         drawer.appendChild(Tree.<String>create("Menu")
-                .appendChild(TreeItem.create(Icons.widgets(), "Menu 1 qq"))
+                .appendChild(TreeItem.create(Icons.widgets(), "Menu 1 "))
                 .appendChild(TreeItem.create(Icons.widgets(), "Menu 2 "))
                 .addSelectionListener((source, selection) -> {
                     layout.withContent((parent1, content) -> {
